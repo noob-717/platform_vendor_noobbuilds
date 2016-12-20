5620a27adb9ff21dc5de09da7ec2aef562619b66
calebcabob <calphonic@gmail.com>
2016-12-20 12:41:03 -0500

    Add calphonic phone.mk - thanks @fwhem

---
+++ configs/calphonic_phone.mk
@@ -0,0 +1,16 @@
+# calphonic phone make file
+
+# Include common make file
+include vendor/calphonic/configs/common.mk
+
+# Phone packages
+PRODUCT_PACKAGES += \
+    Stk \
+    CellBroadcastReceiver
+    
+# Phone build prop overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tethering.noprovisioning=true \
+    persist.sys.dun.override=0 \
+    keyguard.no_require_sim=true \
+    ro.com.android.dataroaming=false
