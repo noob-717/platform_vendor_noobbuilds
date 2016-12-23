87204f2aa5eea369847bf59e1a32361247d0ea00
calebcabob <calphonic@gmail.com>
2016-12-23 16:21:15 -0500

    Update fingerprint to Dec 2016 - NPF26F

--- products/angler.mk
+++ products/angler.mk
@@ -13,3 +13,9 @@ PRODUCT_BRAND := google
 PRODUCT_DEVICE := angler
 PRODUCT_MODEL := Nexus 6P
 PRODUCT_MANUFACTURER := Huawei
+
+# Add Fingerprints/Build Description
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:7.1.1/NPF26F/3468883:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 NPF26F 3468883 release-keys"
